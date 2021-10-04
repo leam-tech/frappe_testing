13edd82378ad5e20454cff1c7d016dda55a67c22
Fahim Ali Zain <fahimalizain@gmail.com>
2021-10-04 19:28:38 +0530

    chore: update setup.py version

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
+import re
+import ast
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
-	install_requires = f.read().strip().split("\n")
+    install_requires = f.read().strip().split("\n")
 
-# get version from __version__ variable in frappe_testing/__init__.py
-from frappe_testing import __version__ as version
+# get version from __version__ variable in frappe_graphql/__init__.py
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('frappe_graphql/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
 
 setup(
-	name="frappe_testing",
-	version=version,
-	description="A simpler frappe-test-fixture provider",
-	author="Leam Technology Systems",
-	author_email="info@leam.ae",
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
+    name="frappe_testing",
+    version=version,
+    description="A simpler frappe-test-fixture provider",
+    author="Leam Technology Systems",
+    author_email="info@leam.ae",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_requires
 )
