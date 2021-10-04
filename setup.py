7499de682067bff7e719520c42baf248f7853d6b
Fahim Ali Zain <fahimalizain@gmail.com>
2021-10-04 19:42:21 +0530

    fix: setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     install_requires = f.read().strip().split("\n")
 
-# get version from __version__ variable in frappe_graphql/__init__.py
+# get version from __version__ variable in frappe_testing/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('frappe_graphql/__init__.py', 'rb') as f:
+with open('frappe_testing/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
